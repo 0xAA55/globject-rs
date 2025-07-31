4d8237109c6605a2036423c4c74eae13e6078e40
0xAA55 <weixiaobao213@qq.com>
2025-07-31 18:29:46 +0800

    实现 `DrawCommand::default()`，并添加 `#[repr(packed)]`

--- src/glcmdbuf.rs
+++ src/glcmdbuf.rs
@@ -1,4 +1,8 @@
+#![allow(dead_code)]
 
+use std::default::Default;
+
+#[repr(packed)]
 #[derive(Debug, Clone, Copy)]
 pub struct DrawCommand {
 	element_count: u32,
@@ -6,4 +10,16 @@ pub struct DrawCommand {
 	first_index: u32,
 	base_vertex: i32,
 	base_instance: u32,
+}
+
+impl Default for DrawCommand {
+	fn default() -> Self {
+		Self {
+			element_count: 0,
+			instance_count: 0,
+			first_index: 0,
+			base_vertex: 0,
+			base_instance: 0,
+		}
+	}
 }
