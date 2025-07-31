8d3a3fc0c72fb23c54511f042df687e91944874a
0xAA55 <weixiaobao213@qq.com>
2025-07-31 13:03:26 +0800

    添加 `EditableMesh` 和 `DynamicMesh`，并实现每一个的 `Debug`（跳过打印 `GLCore` 的调试信息）

--- src/mesh.rs
+++ src/mesh.rs
@@ -2,8 +2,12 @@
 
 use glcore::*;
 use crate::glbuffer::*;
+use crate::arraybuffer::*;
+use std::{
+	fmt::{self, Debug, Formatter}
+};
 
-#[derive(Debug, Clone)]
+#[derive(Clone)]
 pub struct Mesh<'a> {
 	glcore: &'a GLCore,
 	vertex_buffer: Buffer<'a>,
@@ -11,4 +15,48 @@ pub struct Mesh<'a> {
 	command_buffer: Option<Buffer<'a>>
 }
 
+#[derive(Clone)]
+pub struct EditableMesh<'a> {
+	glcore: &'a GLCore,
+	vertex_buffer: ArrayBuffer<'a>,
+	instance_buffer: Option<ArrayBuffer<'a>>,
+	command_buffer: Option<ArrayBuffer<'a>>
+}
 
+#[derive(Clone)]
+pub struct DynamicMesh<'a, T: ArrayBufferItem> {
+	glcore: &'a GLCore,
+	vertex_buffer: ArrayBufferDynamic<'a, T>,
+	instance_buffer: Option<ArrayBufferDynamic<'a, T>>,
+	command_buffer: Option<ArrayBufferDynamic<'a, T>>
+}
+
+impl<'a> Debug for Mesh<'a> {
+	fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+		f.debug_struct("Mesh")
+		.field("vertex_buffer", &self.vertex_buffer)
+		.field("instance_buffer", &self.instance_buffer)
+		.field("command_buffer", &self.command_buffer)
+		.finish()
+	}
+}
+
+impl<'a> Debug for EditableMesh<'a> {
+	fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+		f.debug_struct("Mesh")
+		.field("vertex_buffer", &self.vertex_buffer)
+		.field("instance_buffer", &self.instance_buffer)
+		.field("command_buffer", &self.command_buffer)
+		.finish()
+	}
+}
+
+impl<'a, T: ArrayBufferItem> Debug for DynamicMesh<'a, T> {
+	fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+		f.debug_struct("Mesh")
+		.field("vertex_buffer", &self.vertex_buffer)
+		.field("instance_buffer", &self.instance_buffer)
+		.field("command_buffer", &self.command_buffer)
+		.finish()
+	}
+}
