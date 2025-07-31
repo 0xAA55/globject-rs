0c8b77e7678336d078f78537bf900a05fd0f3278
0xAA55 <weixiaobao213@qq.com>
2025-07-31 14:16:50 +0800

    实现各种 `Mesh` 之间的相互转换。

--- src/mesh.rs
+++ src/mesh.rs
@@ -31,6 +31,103 @@ pub struct DynamicMesh<'a, T: ArrayBufferItem> {
 	pub command_buffer: Option<ArrayBufferDynamic<'a, T>>,
 }
 
+impl<'a> Mesh<'a> {
+	pub fn new(glcore: &'a GLCore, vertex_buffer: Buffer<'a>, instance_buffer: Option<Buffer<'a>>, command_buffer: Option<Buffer<'a>>) -> Self {
+		Self {
+			glcore,
+			vertex_buffer,
+			instance_buffer,
+			command_buffer,
+		}
+	}
+}
+
+impl<'a> EditableMesh<'a> {
+	pub fn new(glcore: &'a GLCore, vertex_buffer: ArrayBuffer<'a>, instance_buffer: Option<ArrayBuffer<'a>>, command_buffer: Option<ArrayBuffer<'a>>) -> Self {
+		Self {
+			glcore,
+			vertex_buffer,
+			instance_buffer,
+			command_buffer,
+		}
+	}
+}
+
+impl<'a, T: ArrayBufferItem> DynamicMesh<'a, T> {
+	pub fn new(glcore: &'a GLCore, vertex_buffer: ArrayBufferDynamic<'a, T>, instance_buffer: Option<ArrayBufferDynamic<'a, T>>, command_buffer: Option<ArrayBufferDynamic<'a, T>>) -> Self {
+		Self {
+			glcore,
+			vertex_buffer,
+			instance_buffer,
+			command_buffer,
+		}
+	}
+}
+
+impl<'a> Into<EditableMesh<'a>> for Mesh<'a> {
+	fn into(self) -> EditableMesh<'a> {
+		EditableMesh {
+			glcore: self.glcore,
+			vertex_buffer: self.vertex_buffer.into(),
+			instance_buffer: self.instance_buffer.map(|b|b.into()),
+			command_buffer: self.command_buffer.map(|b|b.into()),
+		}
+	}
+}
+
+impl<'a> Into<Mesh<'a>> for EditableMesh<'a> {
+	fn into(self) -> Mesh<'a> {
+		Mesh {
+			glcore: self.glcore,
+			vertex_buffer: self.vertex_buffer.into(),
+			instance_buffer: self.instance_buffer.map(|b|b.into()),
+			command_buffer: self.command_buffer.map(|b|b.into()),
+		}
+	}
+}
+
+impl<'a, T: ArrayBufferItem> Into<DynamicMesh<'a, T>> for Mesh<'a> {
+	fn into(self) -> DynamicMesh<'a, T> {
+		DynamicMesh {
+			glcore: self.glcore,
+			vertex_buffer: self.vertex_buffer.into(),
+			instance_buffer: self.instance_buffer.map(|b|b.into()),
+			command_buffer: self.command_buffer.map(|b|b.into()),
+		}
+	}
+}
+
+impl<'a, T: ArrayBufferItem> Into<Mesh<'a>> for DynamicMesh<'a, T> {
+	fn into(self) -> Mesh<'a> {
+		Mesh {
+			glcore: self.glcore,
+			vertex_buffer: self.vertex_buffer.into(),
+			instance_buffer: self.instance_buffer.map(|b|b.into()),
+			command_buffer: self.command_buffer.map(|b|b.into()),
+		}
+	}
+}
+
+impl<'a, T: ArrayBufferItem> Into<EditableMesh<'a>> for DynamicMesh<'a, T> {
+	fn into(self) -> EditableMesh<'a> {
+		EditableMesh {
+			glcore: self.glcore,
+			vertex_buffer: self.vertex_buffer.into(),
+			instance_buffer: self.instance_buffer.map(|b|b.into()),
+			command_buffer: self.command_buffer.map(|b|b.into()),
+		}
+	}
+}
+
+impl<'a, T: ArrayBufferItem> Into<DynamicMesh<'a, T>> for EditableMesh<'a> {
+	fn into(self) -> DynamicMesh<'a, T> {
+		DynamicMesh {
+			glcore: self.glcore,
+			vertex_buffer: self.vertex_buffer.into(),
+			instance_buffer: self.instance_buffer.map(|b|b.into()),
+			command_buffer: self.command_buffer.map(|b|b.into()),
+		}
+	}
 }
 
 impl<'a> Debug for Mesh<'a> {
