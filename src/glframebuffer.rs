6644cf2a664a44de39cdf6bb57d3c6ad651b6715
0xAA55 <weixiaobao213@qq.com>
2025-08-01 19:20:55 +0800

    提供最基础的功能实现（待开发）

--- src/glframebuffer.rs
+++ src/glframebuffer.rs
@@ -2,7 +2,19 @@
 #![allow(dead_code)]
 
 use glcore::*;
+use std::{
+	fmt::{self, Debug, Formatter},
+};
 
 pub struct Framebuffer<'a> {
 	pub glcore: &'a GLCore,
+	name: u32,
+}
+
+impl Debug for Framebuffer<'_> {
+	fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+		f.debug_struct("Framebuffer")
+		.field("name", &self.name)
+		.finish()
+	}
 }
