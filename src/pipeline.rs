d5ec7dad76562ff0d5916e627f7667175df2ae9c
0xAA55 <weixiaobao213@qq.com>
2025-08-03 01:00:50 +0800

    调用 `glVertexAttrib?Pointer()` 的时候，需要传入基本元素类型，而非变量类型。

--- src/pipeline.rs
+++ src/pipeline.rs
@@ -136,11 +136,11 @@ impl<M: Mesh> Pipeline<M> {
 						let ptr_param = cur_offset as *const c_void;
 						self.glcore.glEnableVertexAttribArray(location);
 						if attrib_type.is_float() {
-							self.glcore.glVertexAttribPointer(location, p_size as i32, attrib_type.get_type() as u32, do_normalize, stride as i32, ptr_param);
+							self.glcore.glVertexAttribPointer(location, p_size as i32, attrib_type.get_base_type() as u32, do_normalize, stride as i32, ptr_param);
 						} else if attrib_type.is_integer() {
-							self.glcore.glVertexAttribIPointer(location, p_size as i32, attrib_type.get_type() as u32, stride as i32, ptr_param);
+							self.glcore.glVertexAttribIPointer(location, p_size as i32, attrib_type.get_base_type() as u32, stride as i32, ptr_param);
 						} else if attrib_type.is_double() {
-							self.glcore.glVertexAttribLPointer(location, p_size as i32, attrib_type.get_type() as u32, stride as i32, ptr_param);
+							self.glcore.glVertexAttribLPointer(location, p_size as i32, attrib_type.get_base_type() as u32, stride as i32, ptr_param);
 						} else {
 							panic!("Unknown data type of the attrib `{} {field_name}`", attrib_type.get_type());
 						}
