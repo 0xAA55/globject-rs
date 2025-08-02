3bc62e192d11f5bfe75c699fd13dc0b88ddf007f
0xAA55 <weixiaobao213@qq.com>
2025-08-03 00:59:55 +0800

    简化代码，对于 `element_buffer` 使用「有就绑，没有就不绑」的逻辑。

--- src/pipeline.rs
+++ src/pipeline.rs
@@ -341,15 +341,17 @@ impl<'a, M: Mesh> PipelineBind<'a, M> {
 		let mesh = &self.pipeline.mesh;
 		let vertex_buffer = mesh.get_vertex_buffer();
 		let element_buffer = mesh.get_element_buffer();
+		let e_bind = element_buffer.as_ref().map(|b| {
+			assert_eq!(b.buffer.get_target(), BufferTarget::ElementArrayBuffer);
+			b.bind()
+		});
 
 		if let Some(command_buffer) = mesh.get_command_buffer() {
 			assert_eq!(command_buffer.get_target(), BufferTarget::DrawIndirectBuffer);
 			let c_bind = command_buffer.bind();
 			if let Some(element_buffer) = element_buffer {
-				let e_bind = element_buffer.bind();
 				let num_commands = command_buffer.size() / size_of::<DrawElementsCommand>();
 				glcore.glMultiDrawElementsIndirect(mesh.get_primitive() as u32, element_buffer.get_type() as u32, null(), num_commands as i32, size_of::<DrawElementsCommand>() as i32);
-				e_bind.unbind();
 			} else {
 				let num_commands = command_buffer.size() / size_of::<DrawArrayCommand>();
 				glcore.glMultiDrawArraysIndirect(mesh.get_primitive() as u32, null(), num_commands as i32, size_of::<DrawArrayCommand>() as i32);
@@ -360,23 +362,20 @@ impl<'a, M: Mesh> PipelineBind<'a, M> {
 			if let Some(instance_buffer) = mesh.get_instance_buffer() {
 				let num_instances = instance_buffer.size() / self.pipeline.instance_stride;
 				if let Some(element_buffer) = element_buffer {
-					let e_bind = element_buffer.bind();
 					glcore.glDrawElementsInstanced(mesh.get_primitive() as u32, element_buffer.get_num_elements() as i32, element_buffer.get_type() as u32, null(), num_instances as i32);
-					e_bind.unbind();
 				} else {
 					glcore.glDrawArraysInstanced(mesh.get_primitive() as u32, 0, num_vertices as i32, num_instances as i32);
 				}
 			} else {
 				if let Some(element_buffer) = element_buffer {
-					let e_bind = element_buffer.bind();
 					glcore.glDrawElements(mesh.get_primitive() as u32, element_buffer.get_num_elements() as i32, element_buffer.get_type() as u32, null());
-					e_bind.unbind();
 				} else {
 					glcore.glDrawArrays(mesh.get_primitive() as u32, 0, num_vertices as i32);
 				}
 			}
 		}
 
+		e_bind.map(|b|b.unbind());
 		program.unuse();
 		fbo_bind.map(|b|b.unbind());
 	}
