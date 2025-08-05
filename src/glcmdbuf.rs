21b09703fb2ba84e761d4c13336a7e5ba5fcd438
0xAA55 <weixiaobao213@qq.com>
2025-08-05 18:15:47 +0800

    添加 `DispatchIndirectCommand`

--- src/glcmdbuf.rs
+++ src/glcmdbuf.rs
@@ -20,7 +20,15 @@ pub struct DrawElementsCommand {
 	base_instance: u32,
 }
 
+#[derive(Default, Debug, Clone, Copy, PartialEq, Eq)]
+pub struct DispatchIndirectCommand {
+	num_groups_x: u32,
+	num_groups_y: u32,
+	num_groups_z: u32,
+}
+
 pub trait DrawCommand: Default + Clone + Copy + Sized + Debug {}
 
 impl DrawCommand for DrawArrayCommand {}
 impl DrawCommand for DrawElementsCommand {}
+impl DrawCommand for DispatchIndirectCommand {}
