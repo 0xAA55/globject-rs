326023a13aef9b9b7d7ad8aa2bfa0fd033702cc8
0xAA55 <weixiaobao213@qq.com>
2025-08-05 15:35:26 +0800

    新增 `Pipelineset` 可以对多个不同的着色器创建渲染管线，去除 `Pipelineset` 转换回 `Meshset` 的功能。

--- src/meshset.rs
+++ src/meshset.rs
@@ -17,14 +17,30 @@ pub struct Meshset<M: Mesh, Mat: Material> {
 
 #[derive(Debug, Clone)]
 pub struct Pipelineset<V: VertexType, I: VertexType, M: Mesh, Mat: Material> {
-	pub subsets: BTreeMap<String, Rc<Pipeline<V, I, M, Mat>>>,
+	pub subsets: BTreeMap<String, Vec<Rc<Pipeline<V, I, M, Mat>>>>,
 }
 
 impl<V: VertexType, I: VertexType, M: Mesh, Mat: Material> Pipelineset<V, I, M, Mat> {
 	pub fn from_meshset(glcore: Rc<GLCore>, meshset: Meshset<M, Mat>, shader: Rc<Shader>) -> Self {
-		let mut subsets: BTreeMap<String, Rc<Pipeline<V, I, M, Mat>>> = BTreeMap::new();
+		let mut subsets = BTreeMap::new();
 		for (name, mesh) in meshset.subsets.iter() {
-			subsets.insert(name.clone(), Rc::new(Pipeline::new(glcore.clone(), mesh.clone(), shader.clone())));
+			let mut v = Vec::new();
+			v.push(Rc::new(Pipeline::new(glcore.clone(), mesh.clone(), shader.clone())));
+			subsets.insert(name.clone(), v);
+		}
+		Self {
+			subsets
+		}
+	}
+
+	pub fn from_meshset_with_multiple_shaders(glcore: Rc<GLCore>, meshset: Meshset<M, Mat>, shaders: &[Rc<Shader>]) -> Self {
+		let mut subsets = BTreeMap::new();
+		for (name, mesh) in meshset.subsets.iter() {
+			let mut v = Vec::with_capacity(shaders.len());
+			for shader in shaders.iter() {
+				v.push(Rc::new(Pipeline::new(glcore.clone(), mesh.clone(), shader.clone())));
+			}
+			subsets.insert(name.clone(), v);
 		}
 		Self {
 			subsets
@@ -32,22 +48,12 @@ impl<V: VertexType, I: VertexType, M: Mesh, Mat: Material> Pipelineset<V, I, M, 
 	}
 
 	pub fn draw(&self, fbo: Option<&Framebuffer>) {
-		for (_name, pipeline) in self.subsets.iter() {
-			let bind = pipeline.bind();
-			bind.draw(fbo);
-			bind.unbind();
-		}
-	}
-}
-
-impl<V: VertexType, I: VertexType, M: Mesh, Mat: Material> From<Pipelineset<V, I, M, Mat>> for Meshset<M, Mat> {
-	fn from(val: Pipelineset<V, I, M, Mat>) -> Self {
-		let mut subsets: BTreeMap<String, Rc<MeshWithMaterial<M, Mat>>> = BTreeMap::new();
-		for (name, pipeline) in val.subsets.iter() {
-			subsets.insert(name.clone(), pipeline.mesh.clone());
-		}
-		Self {
-			subsets
+		for (_name, pipelines) in self.subsets.iter() {
+			for pipeline in pipelines.iter() {
+				let bind = pipeline.bind();
+				bind.draw(fbo);
+				bind.unbind();
+			}
 		}
 	}
 }
